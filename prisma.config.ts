8f32962461f1509c06e13747c56facfa73df0de8
ProtaSymat <giraultmathys@gmail.com>
2026-02-26 16:58:45 +0100

    edit early

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,7 +7,6 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 export default defineConfig({
-  earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
   migrate: {
     adapter: async () => {
