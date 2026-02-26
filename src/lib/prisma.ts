55e1cbdccf878f46bb676c3775d69a5189e31baf
ProtaSymat <giraultmathys@gmail.com>
2026-02-26 17:42:05 +0100

    prisma 7 ?

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,17 +1,12 @@
-import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
-
-const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-})
-
-const adapter = new PrismaPg(pool)
+import { PrismaClient } from '@prisma/client/wasm'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({ adapter })
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter: new PrismaPg({ connectionString: process.env.DATABASE_URL! }),
+  })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
