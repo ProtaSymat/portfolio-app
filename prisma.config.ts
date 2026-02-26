e2638ea7e13036477a401dcc174029963154fd54
ProtaSymat <giraultmathys@gmail.com>
2026-02-26 17:19:38 +0100

    edit prisma

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,22 +1,6 @@
 import path from 'path'
 import { defineConfig } from 'prisma/config'
-import { PrismaPg } from '@prisma/adapter-pg'
-import pg from 'pg'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
 
 export default defineConfig({
   schema: path.join('prisma', 'schema.prisma'),
-  migrate: {
-    adapter: async () => {
-      const pool = new pg.Pool({
-        connectionString: process.env.DATABASE_URL,
-      })
-      return new PrismaPg(pool)
-    },
-  },
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
 })
